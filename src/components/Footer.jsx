9c2e7c1389c86c7b529d4eac10a1a15400c49113
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-06-17 13:21:37 +0500

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,7 @@ function Footer() {
     <footer className="w-full bg-gray-800 text-white py-4 fixed bottom-0">
       <div className="container-class mx-auto px-4 md:px-20">
         <div className="flex flex-col md:flex-row items-center justify-between">
-          <div className="mb-4 md:mb-0">
-            <h1 className="text-lg font-bold">Footer</h1>
-          </div>
+          <div className="mb-4 md:mb-0"></div>
           <div className="flex space-x-4">
             <a href="#" className="text-gray-400 hover:text-white">
               About
